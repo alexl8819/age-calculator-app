33e512ba7359ba762b0a9334f6dee277fb63f056
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-04-25 02:11:33 -0700

    adjust styling for field labels, set height for form

--- src/lib/AgeForm.svelte
+++ src/lib/AgeForm.svelte
@@ -7,9 +7,9 @@
 
   const handleKeypress = (maxLength) => {
     return (e) => {
-      if (e.keyCode !== 37 && e.keyCode !== 39 && e.keyCode !== 8 && e.keyCode !== ' ' && e.code.includes('Key')) {
+      if (e.keyCode !== 37 && e.keyCode !== 39 && e.keyCode !== 8 && e.keyCode !== 9 && e.keyCode !== ' ' && e.code.includes('Key')) {
         e.preventDefault();
-      } else if (e.target.value.length >= maxLength && e.keyCode !== 37 && e.keyCode !== 39 && e.keyCode !== 8 && e.keyCode !== ' ') {
+      } else if (e.target.value.length >= maxLength && e.keyCode !== 9 && e.keyCode !== 37 && e.keyCode !== 39 && e.keyCode !== 8 && e.keyCode !== ' ') {
         e.preventDefault();
       }
     }
@@ -44,18 +44,18 @@
   <fieldset class="form__fieldset">
     <legend class="sr-only">Enter age date:</legend>
     <div class="form__field">
-      <input type="number" id="day" name="day" class="form__day" min="1" step="1" placeholder="DD" on:input={handleInput('day')} on:keydown={handleKeypress(2)} required />
-      <label for="day" class="form__label">Day</label>
+      <input type="number" id="day" name="day" class="field__day" min="1" step="1" placeholder="DD" on:input={handleInput('day')} on:keydown={handleKeypress(2)} required />
+      <label for="day" class="field__label">Day</label>
       <p></p>
     </div>
     <div class="form__field">
-      <input type="number" id="month" name="month" class="form__month" min="1" step="1" placeholder="MM" on:input={handleInput('month')} on:keydown={handleKeypress(2)} required />
-      <label for="month" class="form__label">Month</label>
+      <input type="number" id="month" name="month" class="field__month" min="1" step="1" placeholder="MM" on:input={handleInput('month')} on:keydown={handleKeypress(2)} required />
+      <label for="month" class="field__label">Month</label>
       <p></p>
     </div>
     <div class="form__field">
-      <input type="number" id="year" name="year" class="form__year" min="1000" max="2023" step="1" placeholder="YYYY" on:input={handleInput('year')} on:keydown={handleKeypress(4)} required />
-      <label for="year" class="form__label">Year</label>
+      <input type="number" id="year" name="year" class="field__year" min="1000" max="2023" step="1" placeholder="YYYY" on:input={handleInput('year')} on:keydown={handleKeypress(4)} required />
+      <label for="year" class="field__label">Year</label>
       <p></p>
     </div>
  </fieldset>
@@ -68,7 +68,8 @@
   .calculator__form {
     display: flex;
     flex-direction: column;
-    margin-top: 15px;
+    height: 130px;
+    margin: 15px;
     border-bottom: 1px solid var(--light-grey);
   }
 
@@ -88,25 +89,34 @@
     width: 33.33%;
   }
 
-  .form__day, .form__month, .form__year {
+  .field__label {
+    letter-spacing: 0.2em;
+    font-size: 0.75rem;
+    color: var(--smokey-grey);
+    text-transform: uppercase;
+    margin-bottom: 2px;
+  }
+
+  .field__day, .field__month, .field__year {
     appearance: textfield;
     padding: 10px;
     border: 1px solid var(--smokey-grey);
     border-radius: 8px;
     font-size: 16px;
+    width: 70px;
+  }
+
+  .form__field input[type=number]::-webkit-inner-spin-button, 
+  .form__field input[type=number]::-webkit-outer-spin-button,
+  .form__field input[type=number] { 
+    -webkit-appearance: none;
+    -moz-appearance: textfield; 
+    margin: 0;
   }
       
   /*.form__day:empty, .form__month:empty, .form__year:empty {
     border: 1px solid var(--light-red);
   }*/
-
-  .form__day, .form__month {
-    width: 85px;
-  }
-
-  .form__year {
-    width: 85px;
-  }
 
   .form__submit {
     display: flex;
